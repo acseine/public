f3dcb0e1441119fc31ece475f18bceb25915389c
acseine <acseine@172.20.10.6>
2023-03-02 11:56:21 +0900

    commit & push

--- test_dwp/src/program/java/test01.java
+++ test_dwp/src/program/java/test01.java
@@ -3,7 +3,12 @@ package program.java;
 public class test01 {
 	
 	public static void main() {
+		
+		
 		System.out.println("Hello World !!!");
+		
+		System.out.println("Hello Korea !!!");
+		
 	}
 
 }
